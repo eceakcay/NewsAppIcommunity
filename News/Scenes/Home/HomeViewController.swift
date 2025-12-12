5ce195f16d304e375b0771689ea48b8492871e27
eceakcay <eceakcay.36@gmail.com>
2025-12-12 13:19:26 +0300

    UIView önemli ve splash ekranı bitti

---
+++ News/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  News
+//
+//  Created by Ece Akcay on 12.12.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ News/Scenes/Splash/SplashViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SplashViewController.swift
+//  News
+//
+//  Created by Ece Akcay on 12.12.2025.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    //MARK: - Properties
+    private let iconImage : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "newspaper.fill")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false 
+        return imageView
+    }()
+    
+    private let newsLabel : UILabel = {
+        let label = UILabel()
+        label.text = "News"
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+}
+
+    //MARK: - Private Methods
+    private extension SplashViewController {
+        func configureView() {
+            view.backgroundColor = .systemBackground
+            addViews()
+            configureLayout()
+        }
+        
+        func addViews() {
+            view.addSubview(iconImage)
+            view.addSubview(newsLabel)
+        }
+        
+        func configureLayout() {
+            iconImage.setupAnchors(
+                centerX: view.centerXAnchor,
+                centerY: view.centerYAnchor,
+                width: 216,
+                height: 216
+            )
+            
+            newsLabel.setupAnchors(
+                top: iconImage.bottomAnchor,
+                paddingTop: 16,
+                leading: view.leadingAnchor,
+                trailing: view.trailingAnchor
+            )
+        }
+    
+}
+
+#Preview {
+    SplashViewController()
+}
